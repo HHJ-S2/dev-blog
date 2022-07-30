a2c66a4e7d532fbd05c5f4d3d7a7e3b8ac8791a4
HHJ-S2 <berdle0122@gmail.com>
2022-07-30 16:35:51 +0900

    add: tailwindCss 환경설정 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,7 @@ module.exports = {
         stripQueryString: true,
       },
     },
+    'gatsby-plugin-postcss',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
